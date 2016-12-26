906acab9a73023bcbfcaa411a42859931354be9d
Pedro Martins <mourao.martins@gmail.com>
2016-12-26 11:05:57 +0000

    Add project files.

---
+++ CommunityNetPortoAngular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityNetPortoAngular.Startup))]
+namespace CommunityNetPortoAngular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
